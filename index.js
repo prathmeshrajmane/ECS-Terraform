98b032b5d3a7901adb6b1761eb19ac3e384d4702
prathmesh-rajmane <109971130+prathmesh-rajmane@users.noreply.github.com>
2023-03-27 13:25:03 +0530

    adding again

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Welcome This Page is running on ECR Farget'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
