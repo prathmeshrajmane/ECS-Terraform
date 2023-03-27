12b0620cd0e7646ac49c7e6e884e31433e762762
prathmesh-rajmane <109971130+prathmesh-rajmane@users.noreply.github.com>
2023-03-27 13:34:59 +0530

    adding v2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Welcome This Page is running on ECR Farget'))
+app.get('/', (req, res) => res.send('Welcome This Page is running on ECR Farget:v1'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
